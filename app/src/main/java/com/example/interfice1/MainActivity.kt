e0473df588fb5c8022b0cf242e088cabe4e70ef2
alubar804 <alubar804@ieselcaminas.org>
2021-09-23 16:23:09 +0200

    inicial

---
+++ app/src/main/java/com/example/interfice1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.interfice1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val jugador = findViewById(R.id.button2) as Button
+
+        jugador.setOnClickListener{lanzarNewPlayer()}
+
+    }
+
+    private fun lanzarNewPlayer (){
+        val i = Intent(this,NewPlayer::class.java)
+        startActivity(i)
+    }
+
+}
