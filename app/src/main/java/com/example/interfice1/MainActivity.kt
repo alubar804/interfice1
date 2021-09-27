5697203d5aa3138685c6c6474f8aa27bab37c241
alu10216804 <alubar804@ieselcaminas.org>
2021-09-27 20:29:05 +0200

    27/9

--- app/src/main/java/com/example/interfice1/MainActivity.kt
+++ app/src/main/java/com/example/interfice1/MainActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val titulo = findViewById<TextView>(R.id.titulo)
+
+        titulo.typeface = ResourcesCompat.getFont(this, R.font.courgetteregular)
 
         val jugador = findViewById(R.id.button2) as Button
 
