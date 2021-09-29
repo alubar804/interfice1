fea9c09c79546d6c152115d85daf8e832c9ba108
alu10216804 <alubar804@ieselcaminas.org>
2021-09-29 20:38:56 +0200

    cosas

--- app/src/main/java/com/example/interfice1/MainActivity.kt
+++ app/src/main/java/com/example/interfice1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.interfice1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toolbar
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         titulo.typeface = ResourcesCompat.getFont(this, R.font.courgetteregular)
         val toolbar=findViewById(R.id.toolbar) as androidx.appcompat.widget.Toolbar
         setSupportActionBar(toolbar)
+        val tooolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(tooolbar)
         val jugador = findViewById(R.id.button2) as Button
 
         jugador.setOnClickListener{lanzarNewPlayer()}
@@ -28,5 +32,18 @@ class MainActivity : AppCompatActivity() {
         val i = Intent(this,NewPlayer::class.java)
         startActivity(i)
     }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.getItemId()) {
+//            (R.id.action_search) -> {return true}
+//            (R.id.action_add) -> {return true}
+//            else -> {return super.onOptionsItemSelected(item)} }
+//    }
+
+
 
 }
