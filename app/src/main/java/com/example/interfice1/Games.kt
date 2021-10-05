b494d71be2e9983b7ef2de443e80abac70bca926
alu10216804 <alubar804@ieselcaminas.org>
2021-10-05 18:17:04 +0200

    progreso en 3 2: electric bogaloo

--- app/src/main/java/com/example/interfice1/Games.kt
+++ app/src/main/java/com/example/interfice1/Games.kt
@@ -3,6 +3,7 @@ package com.example.interfice1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CheckBox
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_games.*
 
@@ -15,35 +16,30 @@ class Games : AppCompatActivity() {
     }
     fun acto(){
         var palabras = ""
+        val seleccionados = mutableListOf(String())
         var elarray = arrayListOf<CheckBox>(checkBox1,checkBox2,checkBox3,checkBox4)
+
         for (i in elarray){
-            if (i++ == arrayString.length) {
-                // end
+            if (i.isChecked) {
+                seleccionados.add(i.text.toString())
             }
-            if (!palabras.equals("")){
-                palabras += " ,"
+        }
+        var total=1
+        for (j in seleccionados) {
+            if (!palabras.equals("")) {
+                if (total < seleccionados.size) {
+                    palabras += " ,"
+                } else {
+                    palabras += " y "
+                }
             }
-            palabras += i.text.toString()
+            palabras += j
+            total++
         }
-        if (checkBox1.isChecked){
-            palabras += checkBox1.text.toString().plus(" ")
+        if (palabras.equals("")) {
+            palabras = "No se ha seleccionado nada"
         }
-        if (checkBox2.isChecked){
-            if (!palabras.equals("")){
-
-            }
-            palabras += checkBox2.text.toString().plus(" ")
-        }
-        if (checkBox3.isChecked){
-            palabras += checkBox3.text.toString().plus(" ")
-        }
-        if (checkBox4.isChecked){
-            palabras += checkBox4.text.toString().plus(" ")
-        }
-        if (palabras.equals("")){
-            palabras="No se ha seleccionado nada"
-        }else{
-
-        }
+        palabras += "."
+        Toast.makeText(applicationContext,palabras, Toast.LENGTH_LONG).show()
     }
 }//imagewiev+checkbutton>scroll
